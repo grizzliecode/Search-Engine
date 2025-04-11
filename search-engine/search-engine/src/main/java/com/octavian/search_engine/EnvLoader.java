ee2dc959a472f0a773dbbe6cfaa50536926dfc9d
grizzliecode <dominicioangrigorut@gmail.com>
2025-04-12 00:31:53 +0300

    Fix configuration privacy
    Sensitive data is now loaded from an .env secret file

---
+++ search-engine/search-engine/src/main/java/com/octavian/search_engine/EnvLoader.java
@@ -0,0 +1,16 @@
+package com.octavian.search_engine;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EnvLoader {
+    @PostConstruct
+    public void loadENEV(){
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+    }
+}
